80dce18577c6f566de106f99b8f057e62c8a932a
EvertonMirand <everton.mirandav@gmail.com>
2020-08-03 20:13:43 -0300

    Ajusta utilitarios

--- src/pages/MoreOptions/styles.ts
+++ src/pages/MoreOptions/styles.ts
@@ -1,5 +1,36 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View`
+import FAIcons from 'react-native-vector-icons/FontAwesome';
 
+import { BACKGROUND, MORE_OPTION_DEFAULT } from '../../utils/Colors';
+
+export const Container = styled.View`
+  flex: 1;
+  color: ${BACKGROUND};
+`;
+
+export const ButtonContainer = styled.TouchableOpacity`
+  height: 70px;
+  border-width: 1px;
+  border-color: ${MORE_OPTION_DEFAULT};
+  align-items: center;
+  padding: 20px;
+  flex-direction: row;
+`;
+
+export const Header = styled.View`
+  height: 100px;
+  padding: 20px;
+`;
+
+export const Icon = styled(FAIcons).attrs({
+  size: 40,
+  color: MORE_OPTION_DEFAULT,
+})`
+  margin-right: 15px;
+`;
+
+export const Text = styled.Text`
+  font-size: 16px;
+  color: ${MORE_OPTION_DEFAULT};
 `;
--- src/utils/Colors.ts
+++ src/utils/Colors.ts
@@ -5,9 +5,10 @@ const BACKGROUND = 'rgba(255,255,255, 1)';
 const TEXT_FIELD = 'rgba(88, 184, 172, 1)';
 const BUTTON_TEXT = '#fff';
 const ERROR_TEXT = '#ff0033';
-const BORDER = 'rgba(235, 235, 235, 1)';
+const BORDER = 'rgba(200, 200, 200, 1)';
 const CAMERA_BACKGROUND = '#000';
 const CAMERA_PRIMARY = '#fff';
+const MORE_OPTION_DEFAULT = '#bbb';
 
 export {
   BUTTON,
@@ -20,4 +21,5 @@ export {
   PRIMARY_COLOR,
   CAMERA_BACKGROUND,
   CAMERA_PRIMARY,
+  MORE_OPTION_DEFAULT,
 };
