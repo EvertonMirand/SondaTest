55bf144addaf1f475b14dac1d2aa7a47711bd57d
EvertonMirand <everton.mirandav@gmail.com>
2020-08-04 19:18:05 -0300

    Tirando o style-inline

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import { View, StatusBar, SafeAreaView } from 'react-native';
+import { View, StatusBar, StyleSheet } from 'react-native';
 import AppRoutes from './routes/AppRoutes';
 
 const App = () => {
   return (
-    <View style={{ flex: 1 }}>
+    <View style={styles.container}>
       <StatusBar barStyle="dark-content" backgroundColor="transparent" />
 
       <AppRoutes />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 export default App;
