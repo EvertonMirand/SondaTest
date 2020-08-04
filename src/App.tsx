163c87329ea446e29be53427fb8c199119c997e8
EvertonMirand <everton.mirandav@gmail.com>
2020-08-04 19:18:33 -0300

    Corrigindo erro do eslint

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,13 @@ import React from 'react';
 import { View, StatusBar, StyleSheet } from 'react-native';
 import AppRoutes from './routes/AppRoutes';
 
-const App = () => {
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+const App = (): JSX.Element => {
   return (
     <View style={styles.container}>
       <StatusBar barStyle="dark-content" backgroundColor="transparent" />
@@ -12,11 +18,5 @@ const App = () => {
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
 
 export default App;
